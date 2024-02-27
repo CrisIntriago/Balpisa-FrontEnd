da583cb2b6a9193bec418162cfccf2b3fa707f96
jairrami06 <jairrami@espol.edu.ec>
2024-02-27 10:42:40 -0500

    Cabecera de reportes

--- src/paginas/Reportes.jsx
+++ src/paginas/Reportes.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Reportes = () => {
-  return (
-    <div>
-      Reportes
-    </div>
-  )
-}
+  const [fechaInicio, setFechaInicio] = useState('');
+  const [fechaFin, setFechaFin] = useState('');
+  // const [tablaVisible, setTablaVisible] = useState(false);
 
-export default Reportes
+  const handleBuscarClick = () => {
+    if (fechaInicio !== '' && fechaFin !== '') {
+      console.log(fechaInicio, fechaFin); 
+      // setTablaVisible(true);
+    } else {
+      alert("Por favor, completa todas las secciones requeridas.");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center bg-gray-100 pb-5">
+      <p className="font-bold text-3xl mt-10 text-center md:w-1/2 mx-auto pb-10 ">Reporte de movimientos por fecha</p>
+      <div className="flex justify-between my-4 mx-4 md:mx-auto w-full md:w-3/4 lg:w-1/3">
+
+        <input 
+          type="date" 
+          value={fechaInicio} 
+          onChange={(e) => setFechaInicio(e.target.value)}
+          className="border-2 border-gray-300 p-2 rounded w-60"
+        />
+
+        <input 
+          type="date" 
+          value={fechaFin} 
+          onChange={(e) => setFechaFin(e.target.value)}
+          className="border-2 border-gray-300 p-2 rounded w-60"
+        />
+      </div>
+      
+      <button
+        onClick={handleBuscarClick}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 my-2 px-4 rounded"
+      >
+        Buscar
+      </button>
+
+      {/* Para mstrar la tabla
+      {tablaVisible && (
+        <div>Contenido de la tabla</div>
+      )}
+      */}
+    </div>
+  );
+};
+
+export default Reportes;
