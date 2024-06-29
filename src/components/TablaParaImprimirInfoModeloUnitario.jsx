685e96924f5768269915f15d2700ea5e43e45e68
jairrami <jairrami@espol.edu.ec>
2024-06-29 00:00:29 -0500

    Cambios para imprimir Modelos

--- src/components/TablaParaImprimirInfoModeloUnitario.jsx
+++ src/components/TablaParaImprimirInfoModeloUnitario.jsx
@@ -102,64 +102,6 @@ const TablaParaImprimirInfoModeloUnitario = forwardRef(
           </tbody>
         </table>
 
-        <p className="font-bold text-2xl mt-10 text-center md:w-1/2 mx-auto pb-10 ">
-          Planchas Del Modelo
-        </p>
-
-        <thead className="text-center bg-primary">
-          <tr>
-            <th className={TdStyle.ThStyle}>Bodega</th>
-            <th className={TdStyle.ThStyle}>COD</th>
-            <th className={TdStyle.ThStyle}>Alto</th>
-            <th className={TdStyle.ThStyle}>Ancho</th>
-            <th className={TdStyle.ThStyle}>D1A</th>
-            <th className={TdStyle.ThStyle}>D1B</th>
-            <th className={TdStyle.ThStyle}>D2A</th>
-            <th className={TdStyle.ThStyle}>D2B</th>
-            <th className={TdStyle.ThStyle}>D3A</th>
-            <th className={TdStyle.ThStyle}>D3B</th>
-            <th className={TdStyle.ThStyle}>
-              Total m<sup>2</sup>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {planchas.map(
-            ({
-              id,
-              nombre,
-              alto,
-              ancho,
-              despunte1A,
-              despunte1B,
-              despunte2A,
-              despunte2B,
-              despunte3A,
-              despunte3B,
-            }) => (
-              <tr key={id}>
-                <td className={TdStyle.TdStyle}>{bodegaSeleccionada}</td>
-                <td className={TdStyle.TdStyle2}>{nombre}</td>
-                <td className={TdStyle.TdStyle}>{alto}</td>
-                <td className={TdStyle.TdStyle2}>{ancho}</td>
-                <td className={TdStyle.TdStyle}>{despunte1A}</td>
-                <td className={TdStyle.TdStyle2}>{despunte1B}</td>
-                <td className={TdStyle.TdStyle}>{despunte2A}</td>
-                <td className={TdStyle.TdStyle2}>{despunte2B}</td>
-                <td className={TdStyle.TdStyle}>{despunte3A}</td>
-                <td className={TdStyle.TdStyle2}>{despunte3B}</td>
-                <td className={TdStyle.TdStyle}>
-                  {(
-                    alto * ancho -
-                    despunte1A * despunte1B -
-                    despunte2A * despunte2B -
-                    despunte3A * despunte3B
-                  ).toFixed(2)}
-                </td>
-              </tr>
-            )
-          )}
-        </tbody>
       </div>
     );
   }
